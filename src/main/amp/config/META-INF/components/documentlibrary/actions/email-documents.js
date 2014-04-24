675541167e83447c4a1fdaa5d55558cd3ca4d082
Mark Clarke <mark@jumpingbean.co.za>
2014-04-24 18:21:26 +0200

    adding option to email without history

--- src/main/amp/config/META-INF/components/documentlibrary/actions/email-documents.js
+++ src/main/amp/config/META-INF/components/documentlibrary/actions/email-documents.js
@@ -19,63 +19,97 @@
 (function() {
     YAHOO.Bubbling.fire("registerAction",
             {
-                actionName: "onActionSendDocumentsViaEmail",
-                fn: function onActionSendDocumentsViaEmail(file) {
+                actionName: "onActionSendDocumentsViaEmailWithHistory",
+                fn: function onActionSendDocumentsViaEmailWithHistory(file) {
                     var dlg = new Alfresco.module.SimpleDialog(this.id +
-                            "-email-documents-" +
+                            "-email-documents-history" +
                             Alfresco.util.generateDomId());
                     var templateUrl = YAHOO.lang.substitute(
                             Alfresco.constants.URL_SERVICECONTEXT +
-                            "components/form?itemKind={itemKind}&itemId={itemId}&destination={destination}&mode={mode}&submitType={submitType}&showCancelButton=true",
+                            "components/form?itemKind={itemKind}&itemId={itemId}&destination={destination}&mode={mode}&submitType={submitType}&showCancelButton={showCancelButton}",
                             {
                                 itemKind: "action",
                                 itemId: "emailDocumentsWithHistoryAction",
                                 mode: "create",
                                 submitType: "json",
+                                showCancelButton: true,
+                                htmlid: this.parentId,
                             });
 
-                    dlg.setOptions(
-                            {
-                                width: "50em",
-                                templateUrl: templateUrl,
-                                onSuccess: {
-                                    fn: function success(response) {
-                                        Alfresco.util.PopupManager.displayMessage("success");
-                                    }
-                                },
-                                onFailure: {
-                                    fn: function fail(response) {
-                                        Alfresco.util.PopupManager.displayMessage("fail");
-                                    }
-                                },
-                                onTemplateLoaded: {
-                                    fn: function templateLoaded(response) {
-                                        Alfresco.util.PopupManager.displayMessage(reponse);
-                                    }
-                                },
-                                doBeforeDialogShow: {
-                                    fn: function doBeforeDialogShow(form, dialog) {
-                                        try {
-                                            if (file.jsNode.hasAspect("jb:emailTemplate")) {
-                                                var from = dialog.id + "_prop_from";
-                                                YAHOO.util.Dom.get(from).value = file.jsNode.properties["jb:from"];
-                                                var subject = dialog.id + "_prop_subject";
-                                                YAHOO.util.Dom.get(subject).value = file.jsNode.properties["jb:subject"];
-                                                var body = dialog.id + "_prop_body";
-                                                YAHOO.util.Dom.get(body).value = file.jsNode.properties["jb:body"];
-                                                var convert = dialog.id + "_prop_convert-entry";
-                                                YAHOO.util.Dom.get(convert).checked = file.jsNode.properties["jb:convert"];
-                                            }else{
-                                                var convert = dialog.id + "_prop_convert-entry";
-                                                YAHOO.util.Dom.get(convert).checked = true;                                                
-                                            }
-                                        } catch (ex) {
-                                            console.log("error retrieving email template..."+ex.message);
-                                        }
-                                    }
-                                },
-                            });
+                    addOptions(dlg, file);
+                    dlg.options.actionUrl = Alfresco.constants.PROXY_URI + "emailDocumentsWithHistoryAction?site=" + page.url.templateArgs.site;
+                    dlg.options.templateUrl = templateUrl;
                     dlg.show();
                 }
             });
+
+    YAHOO.Bubbling.fire("registerAction",
+            {
+                actionName: "onActionSendDocumentsViaEmail",
+                fn: function onActionSendDocumentsViaEmail(file) {
+                    var dlg = new Alfresco.module.SimpleDialog(this.id +
+                            "-email-documents" +
+                            Alfresco.util.generateDomId());
+                    var templateUrl = YAHOO.lang.substitute(
+                            Alfresco.constants.URL_SERVICECONTEXT +
+                            "components/form?itemKind={itemKind}&itemId={itemId}&destination={destination}&mode={mode}&submitType={submitType}&"+
+                            "showCancelButton={showCancelButton}&htmlid={htmlid}",
+                            {
+                                itemKind: "action",
+                                itemId: "emailDocumentsAction",
+                                mode: "create",
+                                submitType: "json",
+                                showCancelButton: true,
+                                htmlid: this.parentId,
+                            });
+
+                    addOptions(dlg, file);
+                    dlg.options.templateUrl = templateUrl;
+                    dlg.show();
+                }
+            });
+
+    function addOptions(dlg, file) {
+        dlg.setOptions(
+                {
+                    width: "50em",
+                    method: Alfresco.util.Ajax.POST,
+                    onSuccess: {
+                        fn: function success(response) {
+                            Alfresco.util.PopupManager.displayMessage("message.email-documents.success");
+                        }
+                    },
+                    onFailure: {
+                        fn: function fail(response) {
+                            Alfresco.util.PopupManager.displayMessage("message.email-documents.failure");
+                        }
+                    },
+                    onTemplateLoaded: {
+                        fn: function templateLoaded(response) {
+                            Alfresco.util.PopupManager.displayMessage(reponse);
+                        }
+                    },
+                    doBeforeDialogShow: {
+                        fn: function doBeforeDialogShow(form, dialog) {
+                            try {
+                                if (file.jsNode.hasAspect("jb:emailTemplate")) {
+                                    var from = dialog.id + "_prop_from";
+                                    YAHOO.util.Dom.get(from).value = file.jsNode.properties["jb:from"];
+                                    var subject = dialog.id + "_prop_subject";
+                                    YAHOO.util.Dom.get(subject).value = file.jsNode.properties["jb:subject"];
+                                    var body = dialog.id + "_prop_body";
+                                    YAHOO.util.Dom.get(body).value = file.jsNode.properties["jb:body"];
+                                    var convert = dialog.id + "_prop_convert-entry";
+                                    YAHOO.util.Dom.get(convert).checked = file.jsNode.properties["jb:convert"];
+                                } else {
+                                    var convert = dialog.id + "_prop_convert-entry";
+                                    YAHOO.util.Dom.get(convert).checked = true;
+                                }
+                            } catch (ex) {
+                                console.log("error retrieving email template..." + ex.message);
+                            }
+                        }
+                    },
+                });
+    }
 })();
